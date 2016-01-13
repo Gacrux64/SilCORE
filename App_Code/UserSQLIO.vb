21ba8dee4487171512367fd181fa291dcd93eb3b
Gacrux64 <matthew.ullyott@gmail.com>
2016-01-13 15:57:55 -0500

    SQL Connection Code changes.
    Updated VB SQL code to connect to the proper db/table for user
    registration.

--- App_Code/UserSQLIO.vb
+++ App_Code/UserSQLIO.vb
@@ -4,15 +4,15 @@ Imports System.Data.SqlClient
 Module PDUserSQLIO
 
     'Database  relatedfunctions
-    Function NewDBConnection(ByRef connectionPerDiem) As Boolean
+    Function NewDBConnection(ByRef connectionBuilder As SqlConnection) As Boolean
         Dim status As Boolean = False
 
         Try
-            Dim connectionStringPerDiem As String = "Data Source=CORE-DB2;Initial Catalog=PERDIUMDB;User ID=SOPSA;Password=SO"
-            connectionPerDiem = New SqlConnection(connectionStringPerDiem)
+            Dim connectionStringBuilder As String = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Builder.mdf;Integrated Security=True"
+            connectionBuilder = New SqlConnection(connectionStringBuilder)
             status = True
         Catch ex As Exception
-            NewErrorMail(ex.ToString(), "The PERDIUMDB Database is unreachable.", "NewDBConnection Function.")
+            NewErrorMail(ex.ToString(), "The Database Is unreachable.", "NewDBConnection Function.")
         End Try
 
         Return status
@@ -20,12 +20,13 @@ Module PDUserSQLIO
 
     Function NewInsertCommand(ByVal sqlConn As SqlConnection) As SqlCommand
 
-        Dim insertComm As SqlCommand = New SqlCommand("INSERT INTO PERDIUMDB.dbo.PERDIUM_USERS (USER_ID, USER_LOGIN, USER_EMAIL, USER_NAME, USER_PERD_PASSWORD, USER_ID_CHECK, USER_SYSTEM) " +
-                            "VALUES ((SELECT MAX(USER_ID)+1 FROM PERDIUM_USERS)," +
-                            "@login," +
+        Dim insertComm As SqlCommand = New SqlCommand("INSERT INTO  Builder.dbo.USER (USER_FIRST, USER_LAST, USER_EMAIL, USER_LOGIN, USER_PASSWORD, USER_SALT, USER_ADMIN) " +
+                            "VALUES (" +
+                            "@firstName," +
+                            "@lastName," +
                             "@email," +
-                            "@name," +
-                            "@perdPassword," +
+                            "@login," +
+                            "@password," +
                             "CONVERT(varbinary(MAX), @salt)," +
                             "@userType)", sqlConn)
         Return insertComm
@@ -36,39 +37,40 @@ Module PDUserSQLIO
         Dim status As Boolean = False
 
         Try
-            Dim adapterPerDiem As New SqlDataAdapter
-            adapterPerDiem.InsertCommand = insertComm
-            adapterPerDiem.InsertCommand.ExecuteNonQuery()
+            Dim adapterBuilder As New SqlDataAdapter
+            adapterBuilder.InsertCommand = insertComm
+            adapterBuilder.InsertCommand.ExecuteNonQuery()
             status = True
         Catch ex As Exception
-            NewErrorMail(ex.ToString(), "The insert command to enter a V1 user into the PERDIUMDB.dbo.PERDIUM_USERS table has failed.", "ExecuteInsertCommand Sub.")
+            NewErrorMail(ex.ToString(), "The insert command to register a new user has failed.", "ExecuteInsertCommand Sub.")
         End Try
 
         Return status
     End Function
 
-    'Entering the new user into the Per Diem system PERDIUM_USER table. If an error occurs along the way, a descriptive e-mail will be sent to the Scribe team and the entire application will exit.
-    Function EnterUser(ByVal name As String, ByVal login As String, ByVal email As String, ByVal salt As Byte(), ByVal password As Byte(), ByVal userType As Integer) As Boolean
+    'Entering the new user into the USER table. If an error occurs along the way, a descriptive e-mail will be sent to the Scribe team and the entire application will exit.
+    Function EnterUser(ByVal firstName As String, ByVal lastName As String, ByVal login As String, ByVal email As String, ByVal salt As Byte(), ByVal password As Byte(), ByVal userType As Integer) As Boolean
 
         Dim status As Boolean = False
 
-        Dim connectionPerDiem As New SqlConnection
+        Dim connectionBuilder As New SqlConnection
         'Create the connection, can throw an error.
-        If NewDBConnection(connectionPerDiem) Then
+        If NewDBConnection(connectionBuilder) Then
 
             'Create the insert statement.
             Dim insertComm As New SqlCommand
-            insertComm = NewInsertCommand(connectionPerDiem)
+            insertComm = NewInsertCommand(connectionBuilder)
 
             'Setting the parameter values of the insert statement.
-            insertComm.Parameters.Add("@login", SqlDbType.NVarChar, -1).Value = login
+            insertComm.Parameters.Add("@firstName", SqlDbType.NVarChar, -1).Value = firstName
+            insertComm.Parameters.Add("@lastName", SqlDbType.NVarChar, -1).Value = lastName
             insertComm.Parameters.Add("@email", SqlDbType.NVarChar, -1).Value = email
-            insertComm.Parameters.Add("@name", SqlDbType.NVarChar, -1).Value = name
-            insertComm.Parameters.Add("@perdPassword", SqlDbType.NVarChar, -1).Value = Convert.ToBase64String(password)
+            insertComm.Parameters.Add("@login", SqlDbType.NVarChar, -1).Value = login
+            insertComm.Parameters.Add("@password", SqlDbType.NVarChar, -1).Value = Convert.ToBase64String(password)
             insertComm.Parameters.Add("@salt", SqlDbType.NVarChar, -1).Value = Convert.ToBase64String(salt)
             insertComm.Parameters.Add("@userType", SqlDbType.Int, -1).Value = userType
 
-            connectionPerDiem.Open()
+            connectionBuilder.Open()
 
             'Execute the insert command using the insert statement, can throw an error.
             If ExecuteInsertCommand(insertComm) Then
@@ -77,7 +79,7 @@ Module PDUserSQLIO
 
             End If
 
-            connectionPerDiem.Close()
+            connectionBuilder.Close()
 
         End If
 
